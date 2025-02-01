9fd3894a7d45e04b684230131bad9503860fc551
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-02-01 17:28:19 -0600

    Refactor index.css and main.jsx to remove unused styles and implement routing with react-router-dom

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast';
 import './index.css'
 import App from './App.jsx'
+import { UserProvider } from './Context/UserContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <Toaster />
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
