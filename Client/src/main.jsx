5422032a739845fbcb5c9e839864596f97164989
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-02-02 20:32:55 -0600

    Add ToastConfig component for customizable toast notifications

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
-import { Toaster } from 'react-hot-toast';
 import './index.css'
 import App from './App.jsx'
 import { UserProvider } from './Context/UserContext.jsx';
+import ToastConfig from './common/styles/ToastConfig.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <Toaster />
+        <ToastConfig />
         <App />
       </UserProvider>
     </BrowserRouter>
