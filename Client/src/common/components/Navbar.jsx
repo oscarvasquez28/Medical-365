3b5e64bcd3e5a0b9e68346ab28f1127910b39b20
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-02-09 21:40:51 -0600

    Implement Navbar component with Material-UI for enhanced navigation and user experience

--- Client/src/common/components/Navbar.jsx
+++ Client/src/common/components/Navbar.jsx
@@ -1,13 +1,77 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
-const Navbar = () => {
+export default function Navbar() {
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </nav>
-  )
+    <Box>
+      <AppBar position="static">
+        <Toolbar className="Toolbar-container" sx = {{backgroundColor: "#fff"}}>
+          <Link to="/" style={{ textDecoration: 'none' }} draggable="false">
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "#1868DB", fontWeight: "700", fontSize: "1.5rem" }}>
+              Medical 365
+            </Typography>
+          </Link>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="black"
+              >
+                <Avatar alt="User Photo" src="https://media.licdn.com/dms/image/v2/D4E03AQHBqGyYyYjg2w/profile-displayphoto-shrink_800_800/B4EZQHLUnBGwAc-/0/1735287196571?e=1744243200&v=beta&t=M2OuNK8wr57EmxDymXpggU_5QvckX1DeI98obJinxa4" />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Navbar
