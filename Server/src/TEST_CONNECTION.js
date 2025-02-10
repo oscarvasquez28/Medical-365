52f3f88fb8cbbaea4837b2342e6c98223300f4f1
PalOntEr <105398631+PalOntEr@users.noreply.github.com>
2025-02-09 21:36:25 -0600

    Add initial server setup with MongoDB test connection and CRUD operations for Colaborador
    #The Colaborador endpoints have yet to be tested

---
+++ Server/src/TEST_CONNECTION.js
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const uri = process.env.CONNECTION_STRING_TO_DB;
+const path = process.env.PATH_TO_CERTIFICATE0;
+
+export default class Context {
+    Connection = null; 
+
+    constructor() {
+        if(this.Connection == null) {
+            this.#Connect();
+        }
+        else 
+            return this.Connection;
+    }
+
+    async #Connect(){
+
+        //It is necessary before to connect to the database set the environment variables and have a valid .pem file
+
+        this.Connection = await mongoose.connect(uri, {
+            tls: true,
+            tlsCertificateKeyFile: path // Update this path to your .pem file
+        })
+        .then(() => {
+            console.log('Connected to MongoDB Atlas');
+        })
+        .catch((err) => {
+            console.error('Error connecting to MongoDB Atlas:', err);
+        });
+        
+    }
+
+    async #Disconnect(){
+        await mongoose.disconnect();
+    }
+} 
+
