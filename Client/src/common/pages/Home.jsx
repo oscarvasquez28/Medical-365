f30f1a078b4eff296f56a37c2191cd95416fb50a
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-02-10 22:45:53 -0600

    Refactor Home component to integrate Dashboard and ModuleCard components for improved layout and functionality

--- Client/src/common/pages/Home.jsx
+++ Client/src/common/pages/Home.jsx
@@ -1,151 +1,34 @@
-import React from 'react';
-import { Container, Button, Box, Typography, TextField, Stack } from '@mui/material';
+import React from 'react'
+import Box from '@mui/material/Box';
+import ModuleCard from '../components/ModuleCard';
+import Dashboard from '../components/Dashboard';
 import { motion } from 'framer-motion';
-import StaticCard from '../components/StaticCard';
 
-export default function Home() {
-  const word1 = "Medical";
-  const word2 = "365";
-
-  // Split the words into arrays of letters
-  const letters1 = word1.split('');
-  const letters2 = word2.split('');
-
-  // Variants for the animation
-  const letterAnimation = {
-    initial: { opacity: 0, y: 20 },
-    animate: { opacity: 1, y: 0 },
-  };
-
+const Home = () => {
   return (
     <>
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <Container maxWidth={false} sx={{ marginTop: '2rem' }}>
-          <Stack direction={{ xs: 'column', md: 'row' }} spacing={2} sx={{ marginTop: '1rem' }}>
-            <Box width={{ xs: '100%', md: '50%' }}>
-              <Typography
-                variant="h2"
-                component="div"
-                sx={{
-                  flexGrow: 1,
-                  fontWeight: '700',
-                  fontSize: '4rem',
-                }}
-              >
-                La excelencia en salud comienza con{' '}
-                <Box component="span" sx={{ display: 'inline-block' }}>
-                  {/* Animation for "Medicall" */}
-                  {letters1.map((letter, index) => (
-                    <motion.span
-                      key={index}
-                      style={{ color: '#1868DB', display: 'inline-block' }}
-                      variants={letterAnimation}
-                      initial="initial"
-                      animate="animate"
-                      transition={{
-                        delay: index * 0.1,
-                        type: 'spring',
-                        stiffness: 200,
-                        damping: 20,
-                        repeat: Infinity,
-                        repeatType: "reverse",
-                        repeatDelay: 2, //  Pause time between repetitions
-                      }}
-                    >
-                      {letter}
-                    </motion.span>
-                  ))}
-                  {/* Space between "Medical" and "365" */}
-                  &nbsp;
-                  {/* Animation for "365" */}
-                  {letters2.map((letter, index) => (
-                    <motion.span
-                      key={index}
-                      style={{ color: '#1868DB', display: 'inline-block' }}
-                      variants={letterAnimation}
-                      initial="initial"
-                      animate="animate"
-                      transition={{
-                        delay: index * 0.1 + letters1.length * 0.1,
-                        type: 'spring',
-                        stiffness: 200,
-                        damping: 20,
-                        repeat: Infinity,
-                        repeatType: "reverse",
-                        repeatDelay: 2,
-                      }}
-                    >
-                      {letter}
-                    </motion.span>
-                  ))}
-                </Box>
-              </Typography>
-            </Box>
-            <Box width={{ xs: '100%', md: '50%' }}>
-              <Box sx={{ display: 'flex', justifyContent: 'center', margin: '1rem' }}>
-                <TextField
-                  fullWidth
-                  label="Email"
-                  variant="outlined"
-                  color="info"
-                  sx={{
-                    '& .MuiOutlinedInput-root': {
-                      borderRadius: '15px',
-                      backgroundColor: 'white',
-                    },
-                    width: { xs: '100%', sm: '70%' },
-                  }}
-                />
-              </Box>
-              <Box sx={{ display: 'flex', justifyContent: 'center', margin: '1rem' }}>
-                <TextField
-                  fullWidth
-                  label="Password"
-                  variant="outlined"
-                  color="info"
-                  sx={{
-                    '& .MuiOutlinedInput-root': {
-                      borderRadius: '15px',
-                      backgroundColor: 'white',
-                    },
-                    width: { xs: '100%', sm: '70%' },
-                  }}
-                />
-              </Box>
-              <Box sx={{ display: 'flex', justifyContent: 'center', margin: '1rem' }}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{
-                    borderRadius: '15px',
-                    width: { xs: '100%', sm: '70%' },
-                    backgroundColor: '#1868DB',
-                    padding: '12px 24px',
-                    fontSize: '1rem',
-                  }}
-                >
-                  Iniciar Sesión
-                </Button>
-              </Box>
-            </Box>
-          </Stack>
-          <Box width={{ xs: '100%', md: '50%' }} marginTop={2}>
-            <Typography variant="h7" component="div" sx={{ flexGrow: 1, fontSize: '1.5rem' }}>
-              La única herramienta de gestión que necesitas para planificar y monitorizar las incidencias de salud en todos los equipos.
-            </Typography>
-          </Box>
-          <Box width={{ xs: '100%' }} marginTop={2} sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-            <StaticCard texto="Monitoreo de incidencias de salud" />
-            <StaticCard texto="Gestión del estado de los colaboradores" />
-            <StaticCard texto="Optimización de procesos de atención médica" />
-            <StaticCard texto="Informes y análisis de datos de salud" />
-          </Box>
-        </Container>
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}>
+        <Dashboard />
+        <Box width={{ xs: '100%' }} marginTop={4} sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+          <ModuleCard
+            title="Tickets"
+            text="Crea, asigna y da seguimiento a tareas e incidencias."
+            route="/tickets" />
+          <ModuleCard
+            title="Reportes"
+            text="Visualiza y genera informes detallados sobre el progreso y desempeño del proyecto."
+            route="/reports" />
+          <ModuleCard
+            title="Colaboradores"
+            text="Gestiona y coordina a los miembros de la organización "
+            route="/collaborators" />
+        </Box>
       </motion.div>
     </>
-  );
+  )
 }
+
+export default Home
