bde12cf3f017ddcb68164315a05fcd683e7cb668
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-02-23 19:41:42 -0600

    Enhance Home component layout with Typography and Container for improved structure

--- Client/src/common/pages/Home.jsx
+++ Client/src/common/pages/Home.jsx
@@ -3,16 +3,22 @@ import Box from '@mui/material/Box';
 import ModuleCard from '../components/ModuleCard';
 import Dashboard from '../components/Dashboard';
 import { motion } from 'framer-motion';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const Home = () => {
   return (
     <>
+    <Box minHeight="100vh">
       <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}>
         <Dashboard />
-        <Box width={{ xs: '100%' }} marginTop={4} sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+        <Container sx={{ marginTop: '4rem' }}>
+          <Typography variant="h6">Mis módulos</Typography>
+        </Container>
+        <Box width={{ xs: '100%' }} marginTop={1} sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
           <ModuleCard
             title="Tickets"
             text="Crea, asigna y da seguimiento a tareas e incidencias."
@@ -27,6 +33,7 @@ const Home = () => {
             route="/collaborators" />
         </Box>
       </motion.div>
+      </Box>
     </>
   )
 }
