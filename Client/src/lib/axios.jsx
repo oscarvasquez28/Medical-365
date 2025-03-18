a5c0028b90fb343042e458ce3e32a377160b2b7a
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-03-18 15:08:36 -0600

    Add axios instance and CollaboratorsAPI service for API interactions

---
+++ Client/src/lib/axios.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_FRONTEND_API_URL}/api`
+})
+
+api.interceptors.request.use((config) => {
+
+  const contentType = config.headers['Content-Type']
+  if (contentType === 'multipart/form-data') {
+    config.headers['Content-Type'] = 'multipart/form-data'
+  } else {
+    config.headers['Content-Type'] = 'application/json'
+  }
+
+  return config
+},
+(error) => {
+  return Promise.reject(error)
+})
+
+export default api
+
