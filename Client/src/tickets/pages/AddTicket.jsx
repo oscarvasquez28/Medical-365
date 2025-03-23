fb5b7092f18efef5dc3a3832b276e63402dff339
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-03-22 21:48:54 -0600

    Add AddTicket and Tickets components with enhanced form fields and improved layout for ticket management

--- Client/src/tickets/pages/AddTicket.jsx
+++ Client/src/tickets/pages/AddTicket.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import CustomBreadcrumb from '../../common/components/CustomBreadcrumb'
+import Stack from '@mui/material/Stack'
+import TextField from '@mui/material/TextField'
+import MenuItem from '@mui/material/MenuItem'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import NavigationButton from '../../common/components/NavigationButton'
 
 const AddTicket = () => {
   const breadcrumbs = [
@@ -14,7 +20,49 @@ const AddTicket = () => {
     <>
       <Container>
         <Box sx={{ flexGrow: 1, mt: '2rem' }}>
-        <CustomBreadcrumb breadcrumbs={breadcrumbs} />
+          <CustomBreadcrumb breadcrumbs={breadcrumbs} />
+          <Typography variant="h4" component="h2" gutterBottom>
+            Agregar Ticket
+          </Typography>
+          <Box sx={{ borderRadius: '1rem', backgroundColor: 'white', padding: '2rem'}}>
+            <Stack direction={{ xs: 'column'}} spacing={3} >
+              <TextField
+                id="outlined-basic"
+                label="Nombre del Ticket"
+                variant="outlined"
+                fullWidth
+              />
+              <TextField
+                id="incidencia"
+                label="Tipo de Incidencia"
+                select
+                fullWidth
+              >
+                <MenuItem value="consulta">Consulta</MenuItem>
+                <MenuItem value="emergencia">Emergencia</MenuItem>
+                <MenuItem value="seguimiento">Seguimiento</MenuItem>
+              </TextField>
+              <TextField
+                id="sintomas"
+                label="Síntomas"
+                select
+                fullWidth
+              >
+                <MenuItem value="consulta">Fiebre</MenuItem>
+                <MenuItem value="emergencia">Dolor de Cabeza</MenuItem>
+                <MenuItem value="seguimiento">Temperatura</MenuItem>
+              </TextField>
+              <TextField
+                id="comentarios"
+                label="Comentarios"
+                fullWidth
+              />
+            </Stack>
+            <Stack direction="row" justifyContent="space-between" sx={{ marginTop: '2rem' }}>
+              <NavigationButton variant="outlined" color="info" Route={'/tickets'} Text={'Regresar'}/>
+              <Button variant="contained" color="primary">Guardar</Button>
+            </Stack>
+          </Box>
         </Box>
       </Container>
     </>
--- Client/src/tickets/pages/Tickets.jsx
+++ Client/src/tickets/pages/Tickets.jsx
@@ -4,54 +4,94 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import DataTable from '../../common/components/DataTable'
 import TextField from '@mui/material/TextField'
-import Autocomplete from '@mui/material/Autocomplete';
+import MenuItem from '@mui/material/MenuItem'
+import Stack from '@mui/material/Stack'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import Grid2 from '@mui/material/Grid2'
 import CustomBreadcrumb from '../../common/components/CustomBreadcrumb'
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: 'ticketName',
+    headerName: 'Nombre del Ticket',
     width: 150,
-    editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
+    field: 'colaborador',
+    headerName: 'Colaborador',
     width: 150,
-    editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: 'tipoIncidencia',
+    headerName: 'Tipo de Incidencia',
     width: 110,
-    editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
+    field: 'prioridad',
+    headerName: 'Prioridad',
     description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    width: 90,
   },
+  {
+    field: 'sintomas',
+    headerName: 'Síntomas',
+    description: 'This column has a value getter and is not sortable.',
+    width: 150,
+  },
+  {
+    field: 'descripcion',
+    headerName: 'Descripción',
+    description: 'This column has a value getter and is not sortable.',
+    width: 150,
+  },
+  {
+    field: 'fecha',
+    headerName: 'Fecha',
+    description: 'This column has a value getter and is not sortable.',
+    width: 100,
+  },
+  {
+    field: 'actions',
+    headerName: 'Acciones',
+    width: 160,
+    renderCell: (params) => (
+        <NavigationButton
+          variant = {'contained'}
+          Text='Editar'
+          color={'info'}
+          Route={`editTicket/${params.id}`}/>
+    ),
+  }
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: 'Daenerys', age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  { id: 10, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  { id: 11, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    id: 1,
+    ticketName: 'Dolor de cabeza',
+    colaborador: 'Juan Pérez',
+    tipoIncidencia: 'Consulta',
+    prioridad: 'Alta',
+    sintomas: 'Dolor persistente',
+    descripcion: 'Dolor de cabeza constante durante 3 días',
+  },
+  {
+    id: 2,
+    ticketName: 'Fiebre alta',
+    colaborador: 'María López',
+    tipoIncidencia: 'Emergencia',
+    prioridad: 'Alta',
+    sintomas: 'Temperatura superior a 39°C',
+    descripcion: 'Fiebre alta acompañada de escalofríos',
+  },
+  {
+    id: 3,
+    ticketName: 'Chequeo general',
+    colaborador: 'Carlos Gómez',
+    tipoIncidencia: 'Preventiva',
+    prioridad: 'Baja',
+    sintomas: 'N/A',
+    descripcion: 'Chequeo médico de rutina',
+  },
 ];
 
 const breadcrumbs = [
@@ -65,49 +105,48 @@ const Tickets = () => {
     <Container>
     <Box sx={{ flexGrow: 1, mt: '2rem' }}>
       <CustomBreadcrumb breadcrumbs={breadcrumbs}/>
-      <Grid2 container spacing={2} sx ={{ paddingTop: '1rem', borderRadius: '1rem'}}>
-        <Grid2 size={12}>
-          <TextField
-            id="outlined-basic"
-            label="Nombre del Ticket"
-            variant="outlined"
-            fullWidth
-          />
-        </Grid2>
-        <Grid2 size={{xs: 6, sm: 6, md:6, lg: 3, xl: 3}}>
-          <Autocomplete
-            disablePortal
-            // options={top100Films}
-            renderInput={(params) => <TextField {...params}
-            label="Colaborador" />}
-            fullWidth
-          />
-        </Grid2>
-        <Grid2 size={{xs: 6, sm: 6, md:6, lg: 3, xl: 3}}>
-        <Autocomplete
-            disablePortal
-            // options={top100Films}
-            renderInput={(params) => <TextField {...params} label="Tipo de Incidencia" />}
-            fullWidth
-          />
-        </Grid2>
-        <Grid2 size={{xs: 6, sm: 6, md:6, lg: 3, xl: 3}}>
-          <Autocomplete
-            disablePortal
-            // options={top100Films}
-            renderInput={(params) => <TextField {...params} label="Prioridad" />}
-            fullWidth
-          />
-        </Grid2>
-        <Grid2 size={{xs: 6, sm: 6, md:6, lg: 3, xl: 3}}>
+      <Stack direction={{ xs: 'row', sm: 'row' }} spacing={3} >
+        <TextField
+          id="outlined-basic"
+          label="Ticket"
+          variant="outlined"
+          fullWidth
+        />
+      </Stack>
+      <Stack direction={{ xs: 'col', sm: 'row' }} spacing={3} sx={{ paddingTop: 3, borderRadius: '1rem'}}>
+        <TextField
+          id="colaborador"
+          label="Colaborador"
+          select
+          fullWidth
+        >
+          <MenuItem value="idCollaborator1">Oscar Vásquez</MenuItem>
+          <MenuItem value="idCollaborator2">Max Zertuche</MenuItem>
+          <MenuItem value="idCollaborator3">Carlos Pecina</MenuItem>
+        </TextField>
+        <TextField
+          id="tipoIncidencia"
+          label="Tipo de Incidencia"
+          select
+          fullWidth
+        >
+          <MenuItem value="Incidencia1">Incidencia 1</MenuItem>
+          <MenuItem value="Incidencia2">Incidencia 2</MenuItem>
+          <MenuItem value="Incidencia3">Incidencia 3</MenuItem>
+        </TextField>
         <DatePicker
-          label="Fecha"
+          label="Fecha Inicio"
           // value={value}
           // onChange={(newValue) => setValue(newValue)}
           sx={{ width: '100%' }}
         />
-        </Grid2>
-      </Grid2>
+         <DatePicker
+          label="Fecha Fin"
+          // value={value}
+          // onChange={(newValue) => setValue(newValue)}
+          sx={{ width: '100%' }}
+        />
+      </Stack>
     </Box>
     <DataTable
       rows={rows}
