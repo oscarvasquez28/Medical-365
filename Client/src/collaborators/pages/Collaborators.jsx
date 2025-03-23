ffba2c4389509c9b9b650dad4e0b793964ead469
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-03-22 21:48:21 -0600

    Refactor Collaborators page to update table columns and enhance filtering with dropdowns for department and status

--- Client/src/collaborators/pages/Collaborators.jsx
+++ Client/src/collaborators/pages/Collaborators.jsx
@@ -4,53 +4,72 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import DataTable from '../../common/components/DataTable'
 import TextField from '@mui/material/TextField'
-import Autocomplete from '@mui/material/Autocomplete';
-import Grid2 from '@mui/material/Grid2'
 import CustomBreadcrumb from '../../common/components/CustomBreadcrumb'
+import Stack from '@mui/material/Stack'
+import MenuItem from '@mui/material/MenuItem'
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: 'id', headerName: 'ID', width: 80 },
   {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
+    field: 'nombre',
+    headerName: 'Nombre',
+    width: 120,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
+    field: 'apellido',
+    headerName: 'Apellido',
+    width: 120,
   },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: 'correo',
+    headerName: 'Correo',
+    width: 120,
+  },
+  {
+    field: 'departamento',
+    headerName: 'Departamento',
+    width: 150,
+  },
+  {
+    field: 'fechaRegistro',
+    headerName: 'Fecha de Registro',
+    width: 150,
+  },
+  {
+    field: 'fechaBaja',
+    headerName: 'Fecha de Baja',
+    width: 150,
+  },
+  {
+    field: 'estado',
+    headerName: 'Estado',
+    width: 100,
+  },
+  {
+    field: 'actions',
+    headerName: 'Acciones',
     width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
+    renderCell: (params) => (
+        <NavigationButton
+          variant = {'contained'}
+          Text='Editar'
+          color={'info'}
+          Route={`editCollaborator/${params.id}`}/>
+    ),
+  }
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: 'Daenerys', age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  { id: 10, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  { id: 11, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, nombre: 'Jon', apellido: 'Snow', correo: 'jon.snow@techcorp.com', departamento: 'Desarrollo de Software', fechaRegistro: '2023-01-01', fechaBaja: '', estado: 'Activo' },
+  { id: 2, nombre: 'Cersei', apellido: 'Lannister', correo: 'cersei.lannister@techcorp.com', departamento: 'Recursos Humanos', fechaRegistro: '2023-02-01', fechaBaja: '', estado: 'Activo' },
+  { id: 3, nombre: 'Jaime', apellido: 'Lannister', correo: 'jaime.lannister@techcorp.com', departamento: 'Seguridad Informática', fechaRegistro: '2023-03-01', fechaBaja: '', estado: 'Activo' },
+  { id: 4, nombre: 'Arya', apellido: 'Stark', correo: 'arya.stark@techcorp.com', departamento: 'Control de Calidad', fechaRegistro: '2023-04-01', fechaBaja: '', estado: 'Activo' },
+  { id: 5, nombre: 'Daenerys', apellido: 'Targaryen', correo: 'daenerys.targaryen@techcorp.com', departamento: 'Innovación y Desarrollo', fechaRegistro: '2023-05-01', fechaBaja: '', estado: 'Activo' },
+  { id: 6, nombre: 'Tyrion', apellido: 'Lannister', correo: 'tyrion.lannister@techcorp.com', departamento: 'Administración', fechaRegistro: '2023-06-01', fechaBaja: '', estado: 'Activo' },
+  { id: 7, nombre: 'Brienne', apellido: 'Tarth', correo: 'brienne.tarth@techcorp.com', departamento: 'Soporte Técnico', fechaRegistro: '2023-07-01', fechaBaja: '', estado: 'Activo' },
+  { id: 8, nombre: 'Sansa', apellido: 'Stark', correo: 'sansa.stark@techcorp.com', departamento: 'Marketing', fechaRegistro: '2023-08-01', fechaBaja: '', estado: 'Activo' },
+  { id: 9, nombre: 'Theon', apellido: 'Greyjoy', correo: 'theon.greyjoy@techcorp.com', departamento: 'Infraestructura', fechaRegistro: '2023-09-01', fechaBaja: '', estado: 'Activo' },
+  { id: 10, nombre: 'Samwell', apellido: 'Tarly', correo: 'samwell.tarly@techcorp.com', departamento: 'Investigación y Desarrollo', fechaRegistro: '2023-10-01', fechaBaja: '', estado: 'Activo' },
 ];
 
 const breadcrumbs = [
@@ -64,32 +83,35 @@ const Collaborators = () => {
     <Container>
     <Box sx={{ flexGrow: 1, mt: '2rem' }}>
       <CustomBreadcrumb breadcrumbs={breadcrumbs}/>
-      <Grid2 container spacing={2}>
-        <Grid2 size={12}>
-          <TextField
-            id="outlined-basic"
-            label="Colaborador"
-            variant="outlined"
-            fullWidth
-          />
-        </Grid2>
-        <Grid2 size={{xs: 6, sm: 6, md:6, lg: 6, xl: 6}}>
-          <Autocomplete
-            disablePortal
-            // options={top100Films}
-            renderInput={(params) => <TextField {...params} label="Departamento" />}
-            fullWidth
-          />
-        </Grid2>
-        <Grid2 size={{xs: 6, sm: 6, md:6, lg: 6, xl: 6}}>
-          <Autocomplete
-            disablePortal
-            // options={top100Films}
-            renderInput={(params) => <TextField {...params} label="Estatus" />}
-            fullWidth
-          />
-        </Grid2>
-      </Grid2>
+      <Stack direction={{ xs: 'row', sm: 'row' }} spacing={3} >
+        <TextField
+          id="outlined-basic"
+          label="Colaborador"
+          variant="outlined"
+          fullWidth
+        />
+      </Stack>
+      <Stack direction={{ xs: 'col', sm: 'row' }} spacing={3} sx={{ paddingTop: 3, borderRadius: '1rem'}}>
+        <TextField
+          id="departamento"
+          label="Departamento"
+          select
+          fullWidth
+        >
+          <MenuItem value="operaciones">Operaciones</MenuItem>
+          <MenuItem value="ingenieria">Ingeniería</MenuItem>
+          <MenuItem value="desarrollo">Desarrollo</MenuItem>
+        </TextField>
+        <TextField
+          id="estatus"
+          label="Estatus"
+          select
+          fullWidth
+        >
+          <MenuItem value="activo">Activo</MenuItem>
+          <MenuItem value="inactivo">Inactivo</MenuItem>
+        </TextField>
+      </Stack>
     </Box>
     <DataTable
       rows={rows}
