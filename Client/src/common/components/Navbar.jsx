ac3c90c9c2708905d72fa06d1d129de1958c3add
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-03-22 21:47:34 -0600

    Add user menu to Navbar with logout functionality

--- Client/src/common/components/Navbar.jsx
+++ Client/src/common/components/Navbar.jsx
@@ -6,28 +6,75 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    console.log('Cerrando sesión...');
+    handleMenuClose();
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar sx = {{backgroundColor: "#fff"}}>
-          <Typography component={Link} to="/" variant="h6" sx={{ flexGrow: 1, color: "#1868DB", fontWeight: "700", fontSize: "1.5rem",  textDecoration: 'none' }}>
+        <Toolbar sx={{ backgroundColor: "#fff" }}>
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            sx={{
+              flexGrow: 1,
+              color: "#1868DB",
+              fontWeight: "700",
+              fontSize: "1.5rem",
+              textDecoration: 'none'
+            }}
+          >
             Medical 365
           </Typography>
-            <div>
+          <div>
             <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                color="black"
-              >
-                <Avatar alt="User Photo" src="https://media.licdn.com/dms/image/v2/D4E03AQHBqGyYyYjg2w/profile-displayphoto-shrink_800_800/B4EZQHLUnBGwAc-/0/1735287196571?e=1744243200&v=beta&t=M2OuNK8wr57EmxDymXpggU_5QvckX1DeI98obJinxa4" />
-              </IconButton>
-            </div>
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+              color="inherit"
+            >
+              <Avatar
+                alt="User Photo"
+                src="https://media.licdn.com/dms/image/v2/D4E03AQHBqGyYyYjg2w/profile-displayphoto-shrink_800_800/B4EZQHLUnBGwAc-/0/1735287196571?e=1744243200&v=beta&t=M2OuNK8wr57EmxDymXpggU_5QvckX1DeI98obJinxa4"
+              />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              <MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
