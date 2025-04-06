88ef787ab855f121ef30ed3159ae3876bb8454a2
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-04-06 13:32:47 -0600

    Add token handling in UserProvider and Login for authentication

--- Client/src/common/pages/Login.jsx
+++ Client/src/common/pages/Login.jsx
@@ -44,6 +44,7 @@ export default function Home() {
         email: response.data.collaborator.email, // Email del usuario
         id: response.data.collaborator.id, // ID del usuario
         logged: true, // Cambia a true porque el usuario ha iniciado sesión
+        token: response.data.token, // Token de autenticación
       });
     } catch (error) {
       console.error(error);
