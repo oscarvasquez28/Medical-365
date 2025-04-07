2873f73b12b6834c0624e35c18e089e1711fa801
PalOntEr <105398631+PalOntEr@users.noreply.github.com>
2025-04-07 15:21:14 -0600

    Update ultimoUsuarioEnModificar field to reference Colaboradores and enhance appointment retrieval with user details

--- Server/routes/appointmentRoutes.js
+++ Server/routes/appointmentRoutes.js
@@ -3,10 +3,12 @@ import express from 'express';
 import Appointments from '../../Data/Citas/Esquema.js'; // Importa el modelo Citas
 import Tickets from '../../Data/Tickets/Esquema.js'; // Importa el modelo Tickets
 import nodemailer from 'nodemailer';
+import Colaborator from '../../Data/Colaboradores/Esquema.js';
 
 const router = express.Router();
 const Appointment = new Appointments();
 const Ticket = new Tickets();
+const Collaborators = new Colaborator();
 
 router.get('/', async (req, res) => {
   try {
@@ -48,7 +50,8 @@ router.get('/table', async (req, res) => {
     const appointmentsWithTickets = await Appointment.model
       .find()
       .populate('ticket')
-      .populate({ path: 'recurso', select: 'nombre' });
+      .populate({ path: 'recurso', select: 'nombre' })
+      .populate({ path: 'ultimoUsuarioEnModificar', select: 'nombre' });
 
       for (const appointment of appointmentsWithTickets) {
         if (appointment.ticket) {
@@ -66,7 +69,7 @@ router.get('/table', async (req, res) => {
           Recurso: appointment.recurso?.nombre,
           Diagnostico: appointment.diagnostico,
           FechaCita: appointment.fechaCita,
-          UltimoUsuarioEnModificar: appointment.ultimoUsuarioEnModificar,
+          UltimoUsuarioEnModificar: appointment.ultimoUsuarioEnModificar.nombre,
           Estatus: appointment.estatus,
         };
     });
