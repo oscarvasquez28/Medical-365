dd9a1bf8e2e1cceb73e66473da90c393f01636a5
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-04-07 02:11:31 -0600

    Add getTicketsList method to TicketsAPI for fetching ticket list

--- Client/src/services/TicketsAPI.js
+++ Client/src/services/TicketsAPI.js
@@ -4,6 +4,9 @@ export default {
   getTickets(){
     return api.get('/tickets')
   },
+  getTicketsList(){
+    return api.get('/tickets/list')
+  },
   postTickets(data){
     return api.post('/tickets', data)
   },
