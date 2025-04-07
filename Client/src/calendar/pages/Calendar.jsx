14e88ba382857fe601352419c474270af638a319
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-04-07 02:11:37 -0600

    Implement appointment fetching in Calendar component; add API integration for retrieving events based on user ID

--- Client/src/calendar/pages/Calendar.jsx
+++ Client/src/calendar/pages/Calendar.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import CustomBreadcrumb from '../../common/components/CustomBreadcrumb'
 import CustomCalendar from '../../common/components/CustomCalendar'
 import dayjs from 'dayjs'
+import AppointmentsAPI from '../../services/AppointmentsAPI'
 
 const Calendar = () => {
-  const events = [
-    {
-      title: "Cita",
-      start: dayjs().toDate(),
-      end: dayjs().toDate(),
-    },
-  ];
+  const { id } = useParams()
+  const [events, setEvents] = useState([]);
+  // const events = [
+  //   {
+  //     title: "Cita",
+  //     start: dayjs().toDate(),
+  //     end: dayjs().toDate(),
+  //   },
+  // ];
+
+  useEffect(() => {
+    getAppointmentsCalendar(id);
+  }, []);
+
+  async function getAppointmentsCalendar(id) {
+    try {
+      const {data} = await AppointmentsAPI.getAppointmentsCalendar(id);
+      const formattedEvents = data.map((event) => ({
+        id: event.id,
+        title: event.title || "Evento sin título", // Asigna un título predeterminado si no existe
+        start: new Date(event.start), // Convierte a objeto Date
+        end: new Date(event.end), // Convierte a objeto Date
+        allDay: event.allDay || false, // Usa el valor de allDay o false por defecto
+      }));
+      setEvents(formattedEvents); // Actualiza el estado con los eventos formateados
+      console.log("Eventos formateados:", formattedEvents);
+    } catch (error) {
+      console.error(error);
+      setAppointments([]);
+    }
+  }
 
   const breadcrumbs = [
     { label: 'Inicio', href: '/' },
