db222bf208bb2d7961fdad1bd5f11f98940b782f
PalOntEr <105398631+PalOntEr@users.noreply.github.com>
2025-04-07 00:33:20 -0600

    Fix default value for creation date in ticket schema

--- Data/Tickets/Esquema.js
+++ Data/Tickets/Esquema.js
@@ -25,7 +25,7 @@ const ticketSchema = new mongoose.Schema({
     },
     fechaCreacion: {
         type: Date,
-        default: Date.now,
+        default: Date.now(),
         required: [true, 'Creation date is required']
     },
     fechaCierre: {
