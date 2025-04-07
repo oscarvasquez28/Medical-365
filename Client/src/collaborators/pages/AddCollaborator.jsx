708800ce6ab3a6545d4a46018d16e326c4e5a668
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-04-07 00:07:08 -0600

    Implement Add and Edit Collaborator functionality with API integration for fetching and posting data

--- Client/src/collaborators/pages/AddCollaborator.jsx
+++ Client/src/collaborators/pages/AddCollaborator.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import CollaboratorsAPI from '../../services/CollaboratorsAPI'
+import DepartmentsAPI from '../../services/DepartmentsAPI'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import CustomBreadcrumb from '../../common/components/CustomBreadcrumb'
@@ -9,13 +12,95 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import NavigationButton from '../../common/components/NavigationButton'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { toast } from 'react-hot-toast';
 
 const AddCollaborator = () => {
+  const [gender, setGender] = useState([]);
+  const [department, setDepartment] = useState([]);
+  const [role, setRole] = useState([]);
+  const navigate = useNavigate()
+
   const breadcrumbs = [
     { label: 'Inicio', href: '/' },
     { label: 'Colaboradores', href: '/collaborators' },
     { label: 'Agregar Colaborador'}
   ]
+
+  const [collaborator, setCollaborator] = useState({
+    name: '',
+    lastName: '',
+    birthDate: null,
+    gender: '',
+    email: '',
+    password: '',
+    department: '',
+    role: '',
+    status: 'Activo',
+    active: '1'
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setCollaborator({ ...collaborator, [name]: value })
+    console.log(collaborator)
+  }
+
+    useEffect(() => {
+      getGender();
+      getDepartments();
+      getRoles();
+    }, []);
+
+  async function getGender() {
+    try {
+      const {data} = await CollaboratorsAPI.getGender();
+      setGender(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setGender([]);
+    }
+  }
+
+  async function getDepartments() {
+    try {
+      const {data} = await DepartmentsAPI.getDepartments();
+      setDepartment(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setDepartment([]);
+    }
+  }
+
+  async function getRoles() {
+    try {
+      const {data} = await CollaboratorsAPI.getRoles();
+      setRole(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setRole([]);
+    }
+  }
+
+  async function postCollaborator() {
+    try {
+      console.log(collaborator);
+      const response = await CollaboratorsAPI.postCollaborator(collaborator);
+      console.log("Colaborador agregado con éxito", response.data);
+      toast.success("Colaborador agregado con éxito")
+      navigate('/collaborators');
+    } catch (error) {
+      console.error(error);
+      toast.error("Error al agregar colaborador")
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    postCollaborator()
+  };
 
   return (
     <>
@@ -32,63 +117,87 @@ const AddCollaborator = () => {
                 label="Nombre"
                 variant="outlined"
                 fullWidth
+                onChange={handleInputChange}
+                name="name"
               />
               <TextField
                 id="outlined-basic"
                 label="Apellido"
                 variant="outlined"
                 fullWidth
+                onChange={handleInputChange}
+                name="lastName"
               />
             </Stack>
             <Stack direction={{ xs: 'column'}} spacing={3} >
-            <DatePicker
+              <DatePicker
                 label="Fecha de Nacimiento"
-                // value={value}
-                // onChange={(newValue) => setValue(newValue)}
+                onChange={(newValue) => setCollaborator({ ...collaborator, birthDate: newValue })}
                 sx={{ width: '100%' }}
               />
+              <TextField
+                id="gender"
+                label="Género"
+                select
+                fullWidth
+                onChange={handleInputChange}
+                name="gender"
+              >
+                {gender.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
               <TextField
                 id="outlined-basic"
                 label="Correo"
                 variant="outlined"
                 fullWidth
+                onChange={handleInputChange}
+                name="email"
               />
               <TextField
                 id="outlined-basic"
                 label="Contraseña"
                 variant="outlined"
                 fullWidth
+                // type="password"
+                onChange={handleInputChange}
+                name="password"
               />
               <TextField
-                id="departamento"
+                id="department"
                 label="Departamento"
                 select
                 fullWidth
+                onChange={handleInputChange}
+                name="department"
               >
-                <MenuItem value="operaciones">Operaciones</MenuItem>
-                <MenuItem value="ingenieria">Ingeniería</MenuItem>
-                <MenuItem value="desarrollo">Desarrollo</MenuItem>
+                {department.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </TextField>
               <TextField
-                id="rol"
-                label="Rol"
+                id="role"
+                label="Role"
                 select
                 fullWidth
+                onChange={handleInputChange}
+                name="role"
               >
-                <MenuItem value="administrador">Administrador</MenuItem>
-                <MenuItem value="gerente">Gerente</MenuItem>
-                <MenuItem value="colaborador">Colaborador</MenuItem>
+                {role.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </TextField>
-              <DatePicker
-                label="Fecha de Registro"
-                // value={value}
-                // onChange={(newValue) => setValue(newValue)}
-                sx={{ width: '100%' }}
-              />
             </Stack>
             <Stack direction="row" justifyContent="space-between" sx={{ marginTop: '2rem' }}>
               <NavigationButton variant="outlined" color="info" Route={'/collaborators'} Text={'Regresar'}/>
-              <Button variant="contained" color="primary">Agregar</Button>
+              <Button variant="contained" color="primary" onClick={handleSubmit}>Agregar</Button>
             </Stack>
           </Box>
         </Box>
--- Client/src/collaborators/pages/EditCollaborator.jsx
+++ Client/src/collaborators/pages/EditCollaborator.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import CollaboratorsAPI from '../../services/CollaboratorsAPI'
+import DepartmentsAPI from '../../services/DepartmentsAPI'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import CustomBreadcrumb from '../../common/components/CustomBreadcrumb'
@@ -9,15 +13,130 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import NavigationButton from '../../common/components/NavigationButton'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
+import dayjs from 'dayjs';
+import { toast } from 'react-hot-toast';
 
 const EditCollaborator = () => {
+  const [gender, setGender] = useState([]);
+  const [department, setDepartment] = useState([]);
+  const [role, setRole] = useState([]);
+  const [status, setStatus] = useState([]);
+  const [collaborator, setCollaborator] = useState({
+    name: '',
+    lastName: '',
+    birthDate: null,
+    gender: '',
+    email: '',
+    password: '',
+    department: '',
+    role: '',
+    status: '',
+    active: '1'
+  });
+  const { id } = useParams()
+  const navigate = useNavigate()
   const breadcrumbs = [
     { label: 'Inicio', href: '/' },
     { label: 'Colaboradores', href: '/collaborators' },
-    { label: 'Agregar Colaborador'}
+    { label: 'Editar Colaborador'}
   ]
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setCollaborator({ ...collaborator, [name]: value })
+    console.log(collaborator)
+  }
+
+    useEffect(() => {
+      getCollaborator(id);
+      getGender();
+      getDepartments();
+      getRoles();
+      getStatus();
+    }, []);
+
+    async function getCollaborator(id) {
+      try {
+        const {data} = await CollaboratorsAPI.getCollaboratorsId(id);
+        setCollaborator({
+          name: data.Nombre || '',
+          lastName: data.Apellido || '',
+          birthDate: data.FechaDeNacimiento ? dayjs(data.FechaDeNacimiento) : null, // Convierte a dayjs
+          gender: data.Genero || '',
+          email: data.Correo || '',
+          password: data.Contraseña || '',
+          department: data.Departamento || '',
+          role: data.Rol || '',
+          status: data.Estado,
+          active: '1'
+        });
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+        setCollaborator([]);
+      }
+    }
+
+  async function getGender() {
+    try {
+      const {data} = await CollaboratorsAPI.getGender();
+      setGender(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setGender([]);
+    }
+  }
+
+  async function getDepartments() {
+    try {
+      const {data} = await DepartmentsAPI.getDepartments();
+      setDepartment(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setDepartment([]);
+    }
+  }
+
+  async function getRoles() {
+    try {
+      const {data} = await CollaboratorsAPI.getRoles();
+      setRole(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setRole([]);
+    }
+  }
+
+  async function getStatus() {
+    try {
+      const {data} = await CollaboratorsAPI.getStatus();
+      setStatus(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setStatus([]);
+    }
+  }
+
+  async function putCollaborator() {
+    try {
+      console.log(collaborator);
+      const response = await CollaboratorsAPI.putCollaborator(id, collaborator);
+      console.log("Colaborador editado con éxito", response.data);
+      toast.success("Colaborador editado con éxito")
+      navigate('/collaborators')
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    putCollaborator()
+  };
 
   return (
     <>
@@ -34,78 +153,110 @@ const EditCollaborator = () => {
                 label="Nombre"
                 variant="outlined"
                 fullWidth
+                value={collaborator.name}
+                onChange={handleInputChange}
+                name="name"
               />
               <TextField
                 id="outlined-basic"
                 label="Apellido"
                 variant="outlined"
                 fullWidth
+                value={collaborator.lastName}
+                onChange={handleInputChange}
+                name="lastName"
               />
             </Stack>
             <Stack direction={{ xs: 'column'}} spacing={3} >
-            <DatePicker
+              <DatePicker
                 label="Fecha de Nacimiento"
-                // value={value}
-                // onChange={(newValue) => setValue(newValue)}
+                value={collaborator.birthDate || null}
+                onChange={(newValue) => setCollaborator({ ...collaborator, FechaDeNacimiento: newValue })}
                 sx={{ width: '100%' }}
               />
+              <TextField
+                id="gender"
+                label="Género"
+                select
+                fullWidth
+                value={collaborator.gender || ''}
+                onChange={handleInputChange}
+                name="gender"
+              >
+                {gender.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
               <TextField
                 id="outlined-basic"
                 label="Correo"
                 variant="outlined"
                 fullWidth
+                value={collaborator.email}
+                onChange={handleInputChange}
+                name="email"
               />
               <TextField
                 id="outlined-basic"
                 label="Contraseña"
                 variant="outlined"
                 fullWidth
+                // type="password"
+                value={collaborator.password}
+                onChange={handleInputChange}
+                name="password"
               />
               <TextField
-                id="departamento"
+                id="department"
                 label="Departamento"
                 select
                 fullWidth
+                value={collaborator.department || ''}
+                onChange={handleInputChange}
+                name="department"
               >
-                <MenuItem value="operaciones">Operaciones</MenuItem>
-                <MenuItem value="ingenieria">Ingeniería</MenuItem>
-                <MenuItem value="desarrollo">Desarrollo</MenuItem>
+                {department.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </TextField>
               <TextField
-                id="rol"
+                id="role"
                 label="Rol"
                 select
                 fullWidth
+                value={collaborator.role || ''}
+                onChange={handleInputChange}
+                name="role"
               >
-                <MenuItem value="administrador">Administrador</MenuItem>
-                <MenuItem value="gerente">Gerente</MenuItem>
-                <MenuItem value="colaborador">Colaborador</MenuItem>
+                {role.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </TextField>
-              <DatePicker
-                label="Fecha de Registro"
-                // value={value}
-                // onChange={(newValue) => setValue(newValue)}
-                sx={{ width: '100%' }}
-              />
               <TextField
-                id="estado"
+                id="status"
                 label="Estado"
                 select
                 fullWidth
+                value={collaborator.status || ''}
+                onChange={handleInputChange}
+                name="status"
               >
-                <MenuItem value="Activo">Activo</MenuItem>
-                <MenuItem value="Inactivo">Inactivo</MenuItem>
+                {status.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </TextField>
-              <DatePicker
-                label="Fecha de Baja"
-                // value={value}
-                // onChange={(newValue) => setValue(newValue)}
-                sx={{ width: '100%' }}
-              />
             </Stack>
             <Stack direction="row" justifyContent="space-between" sx={{ marginTop: '2rem' }}>
               <NavigationButton variant="outlined" color="info" Route={'/collaborators'} Text={'Regresar'}/>
-              <Button variant="contained" color="primary">Agregar</Button>
+              <Button variant="contained" color="primary" onClick={handleSubmit}>Actualizar</Button>
             </Stack>
           </Box>
         </Box>
