7446a62672eee4b41ba0d84219f939cdc6c11d3c
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-04-07 02:11:02 -0600

    Implement appointment creation with ticket, doctor, and risk selection; enhance API integration for fetching tickets, doctors, and risks.

--- Client/src/appointments/pages/AddAppointment.jsx
+++ Client/src/appointments/pages/AddAppointment.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import {React, useState, useEffect, useContext} from 'react'
+import {UserContext} from '../../Context/UserContext.jsx';
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import CustomBreadcrumb from '../../common/components/CustomBreadcrumb'
@@ -9,13 +10,95 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import NavigationButton from '../../common/components/NavigationButton'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import CollaboratorsAPI from '../../services/CollaboratorsAPI'
+import TicketsAPI from '../../services/TicketsAPI'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-hot-toast'
+import AppointmentsAPI from '../../services/AppointmentsAPI'
 
 const AddAppointment = () => {
+  const navigate = useNavigate()
+  const { user } = useContext(UserContext);
+  const [ticket, setTicket] = useState([])
+  const [doctor, setDoctor] = useState([])
+  const [risk, setRisk] = useState([])
+  const [appointment, setAppointment] = useState({
+    ticket: '',
+    doctor: '',
+    risk: '',
+    appointmentDate: null,
+    lastModifiedBy: user.id,
+  })
+
   const breadcrumbs = [
     { label: 'Inicio', href: '/' },
     { label: 'Citas', href: '/appointments' },
     { label: 'Agregar Cita'}
   ]
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setAppointment({ ...appointment, [name]: value })
+    console.log(appointment)
+  }
+
+  useEffect(() => {
+    getTicket();
+    getDoctor();
+    getRisk();
+  }, []);
+
+  async function postAppointment() {
+    try {
+      console.log(appointment);
+      const response = await AppointmentsAPI.postAppointments(appointment);
+      console.log("Cita agregado con éxito", response.data);
+      toast.success("Cita agregado con éxito")
+      navigate('/appointments');
+    } catch (error) {
+      console.error(error);
+      toast.error("Error al agregar Cita")
+    }
+  }
+
+  async function getTicket() {
+    try {
+      const {data} = await TicketsAPI.getTicketsList();
+      setTicket(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setTicket([]);
+    }
+  }
+
+  async function getDoctor() {
+    try {
+      const {data} = await CollaboratorsAPI.getDoctorList();
+      setDoctor(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setDoctor([]);
+    }
+  }
+
+  async function getRisk() {
+    try {
+      const {data} = await AppointmentsAPI.getRisksList();
+      setRisk(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setRisk([]);
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    postAppointment()
+  };
+
 
   return (
     <>
@@ -32,41 +115,53 @@ const AddAppointment = () => {
                 label="Ticket"
                 select
                 fullWidth
+                onChange={handleInputChange}
+                name="ticket"
               >
-                <MenuItem value="Ticket1">Ticket 1</MenuItem>
-                <MenuItem value="Ticket2">Ticket 2</MenuItem>
-                <MenuItem value="Ticket3">Ticket 3</MenuItem>
+                {ticket.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </TextField>
               <TextField
                 id="doctor"
                 label="Doctor"
                 select
                 fullWidth
+                onChange={handleInputChange}
+                name="doctor"
               >
-                <MenuItem value="idCollaborator1">Dr. Oscar Vásquez</MenuItem>
-                <MenuItem value="idCollaborator2">Dr. Max Zertuche</MenuItem>
-                <MenuItem value="idCollaborator3">Dr. Carlos Pecina</MenuItem>
+                {doctor.map((option) => (
+                  <MenuItem key={option.value} value={option.label}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </TextField>
               <TextField
-                id="riesgo"
+                id="risk"
                 label="Riesgo"
                 select
                 fullWidth
+                onChange={handleInputChange}
+                name="risk"
               >
-                <MenuItem value="riesgo1">Alto</MenuItem>
-                <MenuItem value="riesgo2">Medio</MenuItem>
-                <MenuItem value="riesgo3">Bajo</MenuItem>
+                {risk.map((option) => (
+                  <MenuItem key={option.value} value={option.label}>
+                    {option.label}
+                  </MenuItem>
+                ))}
               </TextField>
               <DatePicker
                 label="Fecha de Cita"
-                // value={value}
-                // onChange={(newValue) => setValue(newValue)}
+                value={appointment.appointmentDate} // Asegúrate de que el valor sea un objeto Date o null
+                onChange={(newValue) => setAppointment({ ...appointment, appointmentDate: newValue })} // Actualiza el estado con la nueva fecha
                 sx={{ width: '100%' }}
               />
             </Stack>
             <Stack direction="row" justifyContent="space-between" sx={{ marginTop: '2rem' }}>
               <NavigationButton variant="outlined" color="info" Route={'/appointments'} Text={'Regresar'}/>
-              <Button variant="contained" color="primary">Guardar</Button>
+              <Button variant="contained" color="primary" onClick={handleSubmit}>Guardar</Button>
             </Stack>
           </Box>
         </Box>
--- Client/src/services/AppointmentsAPI.js
+++ Client/src/services/AppointmentsAPI.js
@@ -4,4 +4,13 @@ export default {
   getAppointments(){
     return api.get('/appointments')
   },
+  getRisksList(){
+    return api.get('/appointments/risks/list')
+  },
+  getAppointmentsCalendar(id){
+    return api.get(`/appointments/calendar/${id}`)
+  },
+  postAppointments(data){
+    return api.post('/appointments', data)
+  },
 }
